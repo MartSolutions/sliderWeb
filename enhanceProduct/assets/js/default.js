c9d6bca4014f7a3b24108167a0673062081ccb58
Kevin Martinez <kevinmartinez08.97@hotmail.com>
2024-11-06 12:02:24 -0300

    Template: Multiple Line - Primer Commit

--- enhanceProduct/assets/js/default.js
+++ enhanceProduct/assets/js/default.js
@@ -17,7 +17,6 @@ $(document).ready(function(){
         var percent = (scrollTop * 100) / totalHeight ;
         console.error("Scroll document: "+ scrollTop + 'px' )
         
-        
         //Cuando el scroll aun no ha llegado al segundo contenedor
         if(scrollTop >= 0 && scrollTop <= totalHeight){
             var percent = (scrollTop * 100) / totalHeight;
@@ -25,6 +24,7 @@ $(document).ready(function(){
             $('#first .product').css('right', ((availableW * ( percent/100)))+'px')
             $('#first .product').css('top', 'calc(15vh + '+ (percent / 5)+'%)')
             $('#first .description').css('margin-left', percent+'%')
+            $('#first .description').css('opacity', ( 1 / percent))
             $('#first .btn').css('margin-bottom', '-'+percent+'%')
             $('#two .description').css('margin-left', 100 - percent+'%')
             console.log("Primer contenedor:"+ percent)
@@ -38,26 +38,40 @@ $(document).ready(function(){
             $('#three .description').css('margin-left', -100 + percent+'%')
         //    console.log("Segundo contenedor:"+ percent)           
             var r_str = $('.product').css('right')
-            var t_str = $('.product').css('top')
             var r_num = parseFloat(r_str.replace('px', ''));
-            var t_num = parseFloat(t_str.replace('px', ''));
-            var r_per = (r_num * 100) / bodyW;
-            var t_per = (r_num * 100) / bodyW;
+            var r_per = ((r_num) * 100) / bodyW;
 
             console.log("AvailableW:"+ availableW)    
             console.log("% top del producto:"+ t_per)    
-            console.log("Move to right:"+ (r_per + percent) /100)    
+            console.log("Move to right:"+ r_per * (1 - percent / 100))    
             
-            $('#first .product').css('right', r_per * (1 - percent / 100)+ '%')
-            var rotate = (percent/3) - (1/3)
-            var scale = 1+((percent/100)/2.5)
-            $('#first .product').css('transform', 'scale('+scale+') rotateZ(-'+rotate+'deg)')
-            console.log('rotate: '+ rotate)
+            var right = r_per * (1 - percent / 100)
+            // var top = (t_per * (1 - (percent + 10) / 100))
+            // var top = t_per
+            // console.log('top_save: '+top_save)
+            var rotate = (percent* (1/4)) - (1/3)
+            var scale = 1+((percent/100)/2)
+            $('#first .product').css({
+                'right': right+ '%',
+                'transform': 'scale('+scale+') ',
+                // rotateZ(-'+rotate+'deg)
+                // 'top': 'calc(0.1 * -'+top+'% )',
+            })
+            
         }
 
         // El tercer contenedor se encuentra en el viewport
         if(scrollTop > (2*totalHeight) && scrollTop <= (3*totalHeight)){
-            
+            var percent = ((scrollTop * 100)  / totalHeight) -200 ;
+             
+            var right = r_per * (1 - percent / 100)
+            // var top = (t_per * (1 - (percent + 10) / 100))
+            var rotate = (percent* (1/3)) - (1/3)
+            var scale = 1+((percent/100)/2.5)
+            $('#first .product').css('right', right+ '%')
+            // $('#first .product').css('top', top+'%')
+            $('#first .product').css('transform', 'scale('+scale+') rotateZ(-'+rotate+'deg)')
+        
 
         }
 
